b526496f9635d104d42c85052643e49ab0209b70
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-13 21:51:59 +0000

    clicking

--- branches/Top_Class.py
+++ branches/Top_Class.py
@@ -5,12 +5,15 @@ running =1
 linecolor = 255, 255, 255
 x = y = 0
 bgcolor = 0, 0, 0
+LEFT=1
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running=0
     elif event.type == pygame.MOUSEMOTION:
         x, y = event.pos
+    elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
+        print "You released the left mouse button at (%d, %d)" % event.pos
     screen.fill((bgcolor))
     pygame.draw.line(screen, linecolor, (x, 0), (x, 399))
     pygame.draw.line(screen, linecolor, (0, y), (639, y))
