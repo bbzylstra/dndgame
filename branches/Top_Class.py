cea02073c0ca001205a9f82afd928c92658f1eb4
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-13 21:08:55 +0000

    Initial upload

---
+++ branches/Top_Class.py
@@ -0,0 +1,12 @@
+
+import pygame
+screen = pygame.display.set_mode((640, 400))
+running =1
+while running:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running=0
+    screen.fill((0,0,0))
+    pygame.draw.line(screen, (0, 0, 255), (0, 0), (200, 100))
+    pygame.draw.aaline(screen, (0, 0, 255), (639, 0), (0, 479))
+    pygame.display.flip()
