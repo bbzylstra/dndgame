0eb78122c3ed2026f1c9df04722e4d074805e1fa
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-13 23:01:29 +0000

    Finsihed game text

--- branches/open_screen.py
+++ branches/open_screen.py
@@ -2,7 +2,7 @@ __author__ = 'Brad'
 import pygame,sys,os
 pygame.init()
 screen = pygame.display.set_mode((640, 400))
-running =True
+running =1
 bgcolor = 0, 0, 0
 LEFT=1
 clock = pygame.time.Clock()
@@ -10,17 +10,18 @@ game_font = pygame.font.Font("freesansbold.ttf",15)
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
-        running=False
+        running=0
     elif event.type == pygame.MOUSEMOTION:
         x, y = event.pos
     elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
         print "You released the left mouse button at (%d, %d)" % event.pos
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
-            running = False
+            running = 0
     screen.fill((bgcolor))
     pygame.draw.rect(screen, (255, 0, 0), (169, 17, 300, 69),1)
     game = game_font.render("GAME", True, (255,0, 0), (0, 0, 0))
     screen.blit(game, (299,47) )
     pygame.display.flip()
-    clock.tick(120)
+    clock.tick(200)
+pygame.quit()
