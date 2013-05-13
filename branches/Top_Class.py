0493acd5f1ef629885159a4c090d1bc7da47a501
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-13 21:48:02 +0000

    Initial upload

--- branches/Top_Class.py
+++ branches/Top_Class.py
@@ -2,11 +2,16 @@
 import pygame
 screen = pygame.display.set_mode((640, 400))
 running =1
+linecolor = 255, 255, 255
+x = y = 0
+bgcolor = 0, 0, 0
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running=0
-    screen.fill((0,0,0))
-    pygame.draw.line(screen, (0, 0, 255), (0, 0), (200, 100))
-    pygame.draw.aaline(screen, (0, 0, 255), (639, 0), (0, 479))
+    elif event.type == pygame.MOUSEMOTION:
+        x, y = event.pos
+    screen.fill((bgcolor))
+    pygame.draw.line(screen, linecolor, (x, 0), (x, 399))
+    pygame.draw.line(screen, linecolor, (0, y), (639, y))
     pygame.display.flip()
