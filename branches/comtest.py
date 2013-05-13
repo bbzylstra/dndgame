5641592ab22f8ca948d47c3e82b29c9ff635127c
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-13 23:35:10 +0000

    Fixed random

--- branches/comtest.py
+++ branches/comtest.py
@@ -106,11 +106,11 @@ while True:
         print '\nThe defending', defclass, 'dodged completely!\n'
         break
     elif 3 * defagl >= dodge:
-        atkdmg = atkdmgmod * random.randrange(3,6,1) / 2
+        atkdmg = atkdmgmod * random.randint(3, 6) / 2
         print '\nThe defending', defclass, 'partially dodged, and only suffered', atkdmg,'!\n'
         break
     else:
-        atkdmg = atkdmgmod * random.randrange(3,6,1)
+        atkdmg = atkdmgmod * random.randint(3, 6)
         defhpleft = defhp - atkdmg
         print '\nThe defending', defclass, 'was hit for', atkdmg, 'damage, and has', defhpleft, 'health left!'
         break
