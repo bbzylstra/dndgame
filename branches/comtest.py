9cae31babd95c751f516f75f2ef74a11dc6663d6
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-14 03:46:04 +0000

    Made very small

--- branches/comtest.py
+++ branches/comtest.py
@@ -7,81 +7,39 @@ randmult = random.randint(2,4)
 randmult = int(randmult)
 attacker = raw_input('The name of the offensive combatant: ')
 defender = raw_input('\nThe name of the defensive combatant: ')
-f=open(attacker,'r')
-atkname = f.readline()
-atkrace = f.readline()
-atkclass = f.readline()
-atkhp = f.readline()
-atkmana = f.readline()
-atkstamina = f.readline()
-atkstre = f.readline()
-atkagl = f.readline()
-atkinte = f.readline()
-atkcour = f.readline()
-atkdex = f.readline()
-atkconc = f.readline()
+attack=[]
+defend=[]
+f = open(attacker)
+attack = f.readlines()
 f.close()
-f=open(defender,'r')
-defname = f.readline()
-defrace = f.readline()
-defclass = f.readline()
-defhp = f.readline()
-defmana = f.readline()
-defstamina = f.readline()
-defstre = f.readline()
-defagl = f.readline()
-definte = f.readline()
-defcour = f.readline()
-defdex = f.readline()
-defconc = f.readline()
+f = open(defender)
+defend = f.readlines()
 f.close()
-atkstre = int(atkstre)
-atkagl = int(atkagl)
-atkinte = int(atkinte)
-atkcour = int(atkcour)
-atkdex = int(atkdex)
-atkconc = int(atkconc)
-defhp = int(defhp)
-defstre = int(defstre)
-defagl = int(defagl)
-definte = int(definte)
-defcour = int(defcour)
-defdex = int(defdex)
-defconc = int(defconc)
 
+if attack[2] == ('swordsman\n' or 'berserker\n' or 'knight\n' or 'scout\n') :
+    atkdmgmod = int(attack[6])
+elif attack[2] == ('hunter\n' or 'ranger\n' or 'skirmisher\n'):
+    atkdmgmod = int(attack[7])
+elif attack[2] == ('priest\n' or 'sorcerer\n' or 'mage\n'):
+    atkdmgmod = int(attack[8])
 
-if atkclass == ('swordsman\n' or 'berserker\n' or 'knight\n' or 'scout\n') :
-    atkdmgmod = atkstre
-elif atkclass == ('hunter\n' or 'ranger\n' or 'skirmisher\n'):
-    atkdmgmod = atkagl
-elif atkclass == ('priest\n' or 'sorcerer\n' or 'mage\n'):
-    atkdmgmod = atkinte
-
-if defclass == ('swordsman\n' or 'berserker\n' or 'knight\n' or 'scout\n'):
-    defdmgmod = defstre
-elif defclass == ('hunter\n' or 'ranger\n' or 'skirmisher\n'):
-    defdmgmod = defagl
-elif defclass == ('priest\n' or 'sorcerer\n' or 'mage\n'):
-    defdmgmod = definte
-
+if defend[2] == ('swordsman\n' or 'berserker\n' or 'knight\n' or 'scout\n'):
+    defdmgmod = int(defend[6])
+elif defend[2] == ('hunter\n' or 'ranger\n' or 'skirmisher\n'):
+    defdmgmod = int(defend[7])
+elif defend[2] == ('priest\n' or 'sorcerer\n' or 'mage\n'):
+    defdmgmod = int(defend[8])
 
 dodge = random.randrange(0,100,1)
-if defagl >= dodge:
-    print '\nThe defending', defclass, 'dodged completely!\n'
+if int(defend[8]) >= dodge:
+    print '\nThe defending', defend[2], 'dodged completely!\n'
 
-elif 3 * defagl >= dodge:
+elif 3 * int(defend[8]) >= dodge:
     atkdmg = (atkdmgmod * randmult)/2
-    print '\nThe defending', defclass, 'partially dodged, and only suffered', atkdmg,'damage!\n'
+    print '\nThe defending', str(defend[2]), 'partially dodged, and only suffered', atkdmg,'damage!\n'
 
 else:
     atkdmg = atkdmgmod * randmult
-    defhpleft = defhp - atkdmg
-    print '\nThe defending', defclass, 'was hit for', atkdmg, 'damage, and has', defhpleft, 'health left!'
+    defhpleft = int(defend[3]) - int(atkdmg)
+    print '\nThe defending', defend[2], 'was hit for', atkdmg, 'damage, and has', defhpleft, 'health left!'
 
-
-print randmult, '= random attack multiplier'
-print atkinte, '= attacker intelligence'
-print atkstre, '= attacker strength'
-print atkdmgmod, '= atkdmgmod'
-print atkclass, ' = Class'
-print atkdmg,'= total damage'
