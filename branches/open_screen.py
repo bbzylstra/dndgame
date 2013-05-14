675e28649a9b1115ebae1ae5b8967077f954866d
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-14 00:27:40 +0000

    Added Dynamic FIle

--- branches/open_screen.py
+++ branches/open_screen.py
@@ -16,7 +16,8 @@ while running:
     elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
         print "You released the left mouse button at (%d, %d)" % event.pos
         if (event.pos[0] >=170 and event.pos[0] <=467) and (event.pos[1] >=114 and event.pos[1] <=197) :
-            os.startfile('C:\Users\Brad\PycharmProjects\Test\charactercreation.py')
+            path=os.path.dirname(os.path.abspath(__file__))
+            os.startfile(path+'\charactercreation.py')
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
             running = 0
