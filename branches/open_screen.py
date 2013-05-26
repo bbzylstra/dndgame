ff0cff8fddf3402cc0112ab115bb74444a709d1e
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-05-26 15:33:28 +0000

    added game

--- branches/open_screen.py
+++ branches/open_screen.py
@@ -10,6 +10,19 @@ bgcolor = 0, 0, 0
 LEFT=1
 clock = pygame.time.Clock()
 game_font = pygame.font.Font("freesansbold.ttf",15)
+game=False
+def drawScreen(game):
+    bgcolor = 0, 0, 0
+    print 'Entered drawScreen'
+    if game == True:
+        while True:
+            event = pygame.event.poll()
+            screen.fill((bgcolor))
+            pygame.display.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    break
+    return None
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
@@ -18,7 +31,9 @@ while running:
         x, y = event.pos
     elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
         print "You released the left mouse button at (%d, %d)" % event.pos
-        if (event.pos[0] >=170 and event.pos[0] <=467) and (event.pos[1] >=114 and event.pos[1] <=197) :
+        if (event.pos[0] >=170 and event.pos[0] <=467) and (event.pos[1] >=30 and event.pos[1] <=100) :
+            game=True
+        elif(event.pos[0] >=170 and event.pos[0] <=467) and (event.pos[1] >=114 and event.pos[1] <=197) :
             path=os.getcwd()
             os.startfile(path+'\charactercreation.py')
         elif (event.pos[0] >=5 and event.pos[0] <=150) and (event.pos[1] >=330 and event.pos[1] <=380) :
@@ -29,7 +44,11 @@ while running:
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
             running = 0
+    drawScreen(game)
     screen.fill((bgcolor))
+    pygame.draw.polygon(screen, (255, 0, 0), [(170, 30),(467,30), (467, 100),(170,100)],1)
+    game = game_font.render("Game", True, (255,0, 0), (0, 0, 0))
+    screen.blit(game, (253,50) )
     pygame.draw.polygon(screen, (255, 0, 0), [(170, 114),(467,114), (467, 197),(170,197)],1)
     game = game_font.render("Character Creation", True, (255,0, 0), (0, 0, 0))
     screen.blit(game, (253,156) )
