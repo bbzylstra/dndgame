08aa7300ab9b6fb94f494904b96c84fbecaa2ef8
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-06-14 03:45:03 +0000

    Added transparency for movement.

--- branches/Sprite.py
+++ branches/Sprite.py
@@ -5,14 +5,17 @@ class Ai_Sprite(pygame.sprite.Sprite):
         self.movedistance=5
         pygame.sprite.Sprite.__init__(self)
         self.screen=screen
+        self.health=None
         self.squareNumber2d=(x,y)
         self.cellnumbers=cellnumbers
         self.cellnumbers2d=cellnumbers2d
         self.image=pygame.image.load(imgpath).convert()
         self.rect=self.image.get_rect()
         self.moveSurf=pygame.Surface((500,50))
-        self.moveSurf.fill((0,0,255))
+        self.moveSurf.convert_alpha()
+        self.moveSurf.fill((0,0,255,))
         self.moveSurf=pygame.transform.scale(self.moveSurf,(int(self.cellnumbers2d[1,1][0])-21,int(self.cellnumbers2d[1,1][1])-21))
+        self.moveSurf.set_alpha(100)
         self.selected=False
         drawToScreen.drawToScreen(self.image,self.screen,self.squareNumber2d,self.cellnumbers2d)
     def moveSprite(self,newcellNumber2d):
@@ -27,14 +30,16 @@ class Ai_Sprite(pygame.sprite.Sprite):
                     pass
                 else:
                     ydist=self.movedistance-(z+i)
-                drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]+i,self.squareNumber2d[1]+ydist),self.cellnumbers2d)
-                drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]-ydist,self.squareNumber2d[1]-z),self.cellnumbers2d)
-                drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]-i,self.squareNumber2d[1]+ydist),self.cellnumbers2d)
-                drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]+ydist,self.squareNumber2d[1]-z),self.cellnumbers2d)
+                #drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]+i,self.squareNumber2d[1]+ydist),self.cellnumbers2d)
+                #drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]-ydist,self.squareNumber2d[1]-z),self.cellnumbers2d)
+                #drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]-i,self.squareNumber2d[1]+ydist),self.cellnumbers2d)
+                #drawToScreen.drawToScreen(self.moveSurf,self.screen,(self.squareNumber2d[0]+ydist,self.squareNumber2d[1]-z),self.cellnumbers2d)
                 self.movecells.append((self.squareNumber2d[0]+i,self.squareNumber2d[1]+ydist))
                 self.movecells.append((self.squareNumber2d[0]-ydist,self.squareNumber2d[1]-z))
                 self.movecells.append((self.squareNumber2d[0]-i,self.squareNumber2d[1]+ydist))
                 self.movecells.append((self.squareNumber2d[0]+ydist,self.squareNumber2d[1]-z))
+        for i in set(self.movecells):
+            drawToScreen.drawToScreen(self.moveSurf,self.screen,i,self.cellnumbers2d)
 
     def update(self):
         drawToScreen.drawToScreen(self.image,self.screen,self.squareNumber2d,self.cellnumbers2d)
