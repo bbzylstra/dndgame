9f678bc09d0dfe3c157fd6fb08c2d253fd93ae01
ian.bouldin@gmail.com <ian.bouldin@gmail.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-08-14 19:25:17 +0000

    STILL WORKING ON IT, ITZ TERRIBBUBBLES

--- branches/characterselection.py
+++ branches/characterselection.py
@@ -33,9 +33,9 @@ def characterSelect(screen):
         #Screensize is stored in the screensize tuple. Get x from screensize[0] and y from screensize[1]
         
         f=open(saves_path+selectedSave+"\stats.wagh",'r')
-        name = f.readline()
-        race = f.readline()
-        cclass = f.readline()
+        name = f.readline().rstrip()
+        race = f.readline().rstrip()
+        cclass = f.readline().rstrip()
         hp = int(f.readline())
         mana = int(f.readline())
         stamina = int(f.readline())
@@ -46,20 +46,25 @@ def characterSelect(screen):
         dex = int(f.readline()) 
         conc = int(f.readline())
 
-        xi,yi=screen.get_size()
-        z=(yi/6)-(yi/7)
-        GraphicInput.display_message(screen,'name = '+name,(screensize[0]*.5,screensize[1]*0))
-        GraphicInput.display_message(screen,'hp = '+str(hp),(.5,(z)*1))
-        GraphicInput.display_message(screen,'mana = '+str(mana),(.5,((z)*2)))
-        GraphicInput.display_message(screen,'stamina = '+str(stamina),(.5,((z)*3)))
-        GraphicInput.display_message(screen,'strength = '+str(stre),(.5,((z)*4)))
-        GraphicInput.display_message(screen,'agility = '+str(agl),(.5,((z))*5))
-        GraphicInput.display_message(screen,'intelligence = '+str(inte),(.5,(z)*6))
-        GraphicInput.display_message(screen,'courage = '+str(cour),(.5,((z)*7)))
-        GraphicInput.display_message(screen,'dexterity = '+str(dex),(.5,((z)*8)))
-        GraphicInput.display_message(screen,'concentration = '+str(conc),(.5,(z)*9))
-        GraphicInput.display_message(screen,'race = '+race,(.5,(z)*10))
-        GraphicInput.display_message(screen,'class = '+cclass,(.5,(z)*11))
+        while True:
+            screen.fill((0,0,0))
+            xi, yi = screen.get_size()
+            z = (yi/6)-(yi/7)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or pygame.K_RETURN:
+                break
+            GraphicInput.display_message(screen,'name = '+name,(screensize[0]*.5,screensize[1]*0))
+            GraphicInput.display_message(screen,'hp = '+str(hp),(.5,(z)*1))
+            GraphicInput.display_message(screen,'mana = '+str(mana),(.5,((z)*2)))
+            GraphicInput.display_message(screen,'stamina = '+str(stamina),(.5,((z)*3)))
+            GraphicInput.display_message(screen,'strength = '+str(stre),(.5,((z)*4)))
+            GraphicInput.display_message(screen,'agility = '+str(agl),(.5,((z))*5))
+            GraphicInput.display_message(screen,'intelligence = '+str(inte),(.5,(z)*6))
+            GraphicInput.display_message(screen,'courage = '+str(cour),(.5,((z)*7)))
+            GraphicInput.display_message(screen,'dexterity = '+str(dex),(.5,((z)*8)))
+            GraphicInput.display_message(screen,'concentration = '+str(conc),(.5,(z)*9))
+            GraphicInput.display_message(screen,'race = '+race,(.5,(z)*10))
+            GraphicInput.display_message(screen,'class = '+cclass,(.5,(z)*11))
         #Write front end to display the stats read from the file
 
 
