562ca1fc62040087180ebe3cf20ac4e9086748ac
bradleybaldwin@gmx.com <bradleybaldwin@gmx.com@f80e8b90-4b26-e44d-c56f-852f4a10b6b8>
2013-08-14 15:23:34 +0000

    Added read from file, and comments.

--- branches/characterselection.py
+++ branches/characterselection.py
@@ -1,25 +1,53 @@
 __author__ = 'Ian'
 def characterSelect(screen):
-   import os,pygame,GraphicInput, ctypes
+   import os,pygame,GraphicInput, ctypes, charactercreation
    user32 = ctypes.windll.user32
    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
    screen.fill((0,0,0))
-   running=True
+   name = None
+   race = None
+   cclass = None
+   hp = None
+   mana = None
+   stamina = None
+   stre = None 
+   agl = None 
+   inte = None 
+   cour = None 
+   dex = None 
+   conc = None
+   item = None
+
    current_directory=os.getcwd()
    saves_path=current_directory+'/saves/'
    fileNames=os.listdir(saves_path)
+   selectedSave=GraphicInput.ask(screen,'CharName: ',(300,300))
    GraphicInput.display_message(screen, "names"+str(fileNames), (600, 600))
-   name=GraphicInput.ask(screen,'Enter Name of Character to Select', (300,300))
-   if name == 'FALSE':
-       running=False
-       return None
-   else:
-       pass
-   pygame.display.update()
-   screen.fill((0, 0, 0))
-   while running:
-        xi,yi=screen.get_size()
+   
+   if len(fileNames) == 0:
+       charactercreation.characterCreate(screen)
+       
+   while True:
+        #write graphical front end here to display the names in fileNames
+        #please store the selected file name in a variable called selectedSave
+        #Screensize is stored in the screensize tuple. Get x from screensize[0] and y from screensize[1]
+        f=open(saves_path+selectedSave+"\stats.wagh",'r')
+        name = f.readline()
+        race = f.readline()
+        cclass = f.readline()
+        hp = int(f.readline())
+        mana = int(f.readline())
+        stamina = int(f.readline())
+        stre = int(f.readline())
+        agl = int(f.readline()) 
+        inte = int(f.readline()) 
+        cour = int(f.readline())
+        dex = int(f.readline()) 
+        conc = int(f.readline())
+        #Write front end to display the stats read from the file
         event=pygame.event.poll()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE or pygame.K_RETURN:
                 break
+        
+        
